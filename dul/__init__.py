addd51c0f8eb7ed62776093860333e01073e197c
Hobson Lane <hobsonlane@gmail.com>
2013-01-28 22:21:43 -0800

    add setup and __version__

--- dul/__init__.py
+++ dul/__init__.py
@@ -1,10 +1,36 @@
-# dul.py
-# Data processing UtiLities
+# DUL (Data UtiLities) are great for condensing data into information.
+# Copyright (C) 2013  Hobson Lane <hobson@totalgood.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# used in fencebot for autonomous bitfloor trading
+""" DUL (Data UtiLities) are great for condensing data into information.
+
+More information and examples are available at
+https://github.com/hobsonlane/dul/
+
+"""
+
+# the Canonical way ;)
+version_info = (0, 0, 1, 'alpha', 0)
+
+__version__ = '.'.join(str(i) for i in version_info[0:3])
+version_string = __version__
+
+# used by fence (https://github.com/hobsonlane/fence/) for autonomous trading
 
 # TODO: 
-#       1. map any function to keys, values, or both
+#       1. map any function to keys, values, or both (using `map` or `reduce`)
 #       2. depth argument
 #       3. keys and values argument (to say which or both to apply fun to)
 
